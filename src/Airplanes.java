d3a89383917ec7a1f385d4fe342fb7a52f9467a5
lorenzopeabody28 <lorenzo_peabody28@milton.edu>
2025-11-12 09:04:08 -0500

    added airplane

---
+++ src/Airplanes.java
@@ -0,0 +1,19 @@
+public class Airplanes {
+    /// instance variables
+    /// 1. used in the whole class
+    /// 2. they should be at the top of the class
+    /// 3. have public in front
+    public int capacity;
+    public String name;
+    public String model;
+    public boolean isDoubleDecker;
+    public int length;
+    //constructor
+    public Airplanes(){ //default values
+        capacity = 250;
+        name = "Jacob6";
+        model = "Boeing 737";
+        isDoubleDecker = false;
+        length =157;
+    }
+}
--- src/Factory.java
+++ src/Factory.java
@@ -13,21 +13,48 @@ public class Factory {
         System.out.println("Welcome to Enzo's factory");
         new Factory(); //makes new factory
     }
-    //constructor - is a very special type of method, it has exact same name as class, it has no void, it allows us to make a factory
+    //constructor
+    //1. is a method
+    //2. has same name as class
+    //3. no void when being called upon
     public Factory() {
         isOpen = false;
         //scope for manager is only in the constructor
         String manager = "Mr. Bob";
-
+        factoryInfo();
+        yearFounded = 1944;
         factoryInfo();
         //todo: change the variable yearFounded to be a different value in public Factory constructor
         //call factoryInfo(); again
+        /// type, varName = value
+        //object(fancy variable) of type Airplanes
+        Airplanes plane1 = new Airplanes();
+        plane1.name = "jeff";
+        System.out.println("Name: "+ plane1.name);
+        plane1.capacity = 44;
+        System.out.println("Capacity: "+ plane1.capacity);
+        plane1.isDoubleDecker = false;
+        System.out.println("Double Decker: "+ plane1.isDoubleDecker);
+        plane1.model = "A300";
+        System.out.println("Model: "+ plane1.model);
+        plane1.length = 135;
+        System.out.println("Length: "+ plane1.length + "m");
+
+        Airplanes plane2 = new Airplanes();
+        System.out.println(plane2.capacity);
+        //todo; hw; make 2 more airplane objects
+        //todo; for one of the objects change all the instance variables
+        //todo; for both objects print all the instance variables
+
+
+
+        //todo: give all of plane1's instance vars values
     }
     //sout makes System.out.println()
-    public void factoryInfo () {                       //make a method called factoryInfo
-        System.out.println(yearFounded);
-        System.out.println(factoryName);
-        System.out.println(isOpen);
+    public void factoryInfo () {//make a method called factoryInfo
+        System.out.println("Year founded: "+ yearFounded);
+        System.out.println("Factory name: " + factoryName);
+        System.out.println("Is open: " + isOpen);
         //System.out.println(yearFounded + factoryName + isOpen);
         // more readable option
         //System.out.println("year founded" + yearFounded);
